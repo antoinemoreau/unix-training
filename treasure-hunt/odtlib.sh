7ee7477293cb939830c7b08a48ad4f881aac77f7
Matthieu Moy <Matthieu.Moy@imag.fr>
2016-09-30 14:25:50 +0200

    txt2odt: only use <text:s> for multiple spaces.

--- treasure-hunt/odtlib.sh
+++ treasure-hunt/odtlib.sh
@@ -12,7 +12,7 @@ txt2odt () {
 	-e 's@<@&lt;@g;' \
 	-e 's@>@&gt;@g;' \
 	-e 's@\n@<text:line-break/>@;' \
-	-e 's@( +)@"<text:s text:c=\"".length($1)."\"/>"@eg;' \
+	-e 's@(  +)@"<text:s text:c=\"".length($1)."\"/>"@eg;' \
 	-e 's@\\@\\\\@g;' \
 	-e 's@/@\\/@g;' \
     )
