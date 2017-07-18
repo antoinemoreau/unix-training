c30635e708bd16a81b5d70a69528875c313ef460
Grégory Mounié <Gregory.Mounie@imag.fr>
2017-07-18 13:52:15 +0200

    utf8
    add utf8 in name for apache of lig-enseignement

---
+++ treasure-hunt/etape-H2/index.php
@@ -0,0 +1,7 @@
+<?php
+
+header("X-Etape-Suivante: http://lig-enseignement.imag.fr/jeu-de-piste/aeiouy/etape-H3.txt.utf8");
+
+?>
+
+Non, la reponse n'est pas dans le contenu de la page, seulement dans les en-tetes.
